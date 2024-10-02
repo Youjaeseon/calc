7468a1df7c7d6f8f4b9fa7c429113af2c3c5265a
Ujaesung <wotjd0701@naver.com>
2024-10-02 16:11:59 +0900

    second save

--- calc.py
+++ calc.py
@@ -4,8 +4,16 @@ def add (x,y):
 def miu(x,y):
     return x-y
 
+def mul (x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
 num1 = int(input("첫 번째 정수를 입력하세요: "))
 num2 = int(input("두 번째 정수를 입력하세요: "))
 
 print(f"두 수의 합: {add(num1,num2)}")
 print(f"두 수의 차: {miu(num1,num2)}")
+print(f"두 수의 곱: {mul(num1,num2)}")
+print(f"두 수의 나눗셈: {div(num1,num2)}")
