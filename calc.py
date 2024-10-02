8e908c5d403619e18c4b604036df8646a2b9763d
Ujaesung <wotjd0701@naver.com>
2024-10-02 16:01:52 +0900

    first save

---
+++ calc.py
@@ -0,0 +1,11 @@
+def add (x,y):
+    return x+y
+
+def miu(x,y):
+    return x-y
+
+num1 = int(input("첫 번째 정수를 입력하세요: "))
+num2 = int(input("두 번째 정수를 입력하세요: "))
+
+print(f"두 수의 합: {add(num1,num2)}")
+print(f"두 수의 차: {miu(num1,num2)}")
