e65352fd1698fa81246f681f3ccf780dca41d7a4
Ujaesung <wotjd0701@naver.com>
2024-10-02 16:26:30 +0900

    branch1

--- calc.py
+++ calc.py
@@ -13,7 +13,7 @@ def div(x,y):
 num1 = int(input("첫 번째 정수를 입력하세요: "))
 num2 = int(input("두 번째 정수를 입력하세요: "))
 
-print(f"두 수의 합  feature-branch1 : {add(num1,num2)}")
+print(f"두 수의 합 feature-branch1 : {add(num1,num2)}")
 print(f"두 수의 차: {miu(num1,num2)}")
 print(f"두 수의 곱: {mul(num1,num2)}")
 print(f"두 수의 나눗셈: {div(num1,num2)}")
